2153ad987bd64495a30ff6b6a20d151c353af1c8
Unknown <dayanacuce@gmail.com>
2018-05-12 18:24:52 +0200

    Aggiunta Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CountriesComponent } from './countries/countries.component';
 import { CountryComponent } from './country/country.component';
+import { DashboardComponent} from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'countries', component: CountriesComponent },
   { path: 'country/:id', component: CountryComponent },
-  { path: '', redirectTo: '/countries', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent},
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
